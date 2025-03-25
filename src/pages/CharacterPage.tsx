2368559cf839ca45434688d66f9761ab8578439a
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2025-03-25 01:27:02 +0100

    - Se desinstalo la dependencia "animate.css" del package.json - Se eliminaron todas las importaciones relacionadas en el codigo - Se limpiaron posibles clases como "animate__animated" o "animate__fadeIn" usadas en componentes - Este cambio deja el proyecto mas liviano y enfocado en Tailwind para animaciones

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,11 +1,10 @@
 import { CharacterDetails, Header } from '../components';
-import 'animate.css';
 
 export const CharacterPage = () => {
 	return (
 		<>
 			<Header />
-			<div className='animate__animated animate__fadeInDown'>
+			<div>
 				<CharacterDetails />
 			</div>
 		</>
